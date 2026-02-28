7386e2ee828853a9c5cadb0e227950fc7979c5d1
ashaytiwari <ashaytiwari2001@gmail.com>
2026-02-28 21:28:45 +0530

    feat: integrated todoist add task service in add_task tools

--- main.py
+++ main.py
@@ -8,17 +8,24 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_agent
 # from pydantic_core.core_schema import model_field
+from todoist_api_python.api import TodoistAPI
+
+import todo_actions
 
 load_dotenv()
 
 TODOIST_API_KEY = os.getenv("TODOIST_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+todoist_api = TodoistAPI(TODOIST_API_KEY)
+
 @tool
-def add_task():
+def add_task(task: str, description: str = None):
     """Add a new task to the user's task list. 
-    Use this when the user wants to created or add a new task"""
-    print("Task Added")  
+    Use this when the user wants to add a new task"""
+
+    todoist_api.add_task(content=task, description=description)
+
 
 tools = [add_task]
 
@@ -29,7 +36,7 @@ llm = ChatGoogleGenerativeAI(
 )
 
 system_prompt = "You are a helpful AI assistant, You will help user manage their tasks in Todoist platform."
-user_input = "Add task to buy a milk"
+user_input = "Create a new task as Learn Python"
 
 prompt = ChatPromptTemplate([
   ("system", system_prompt), 
